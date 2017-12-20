a0fc010c0f5806b6a0e419f70b06aaf3b89dcbfc
Vladislav Andrš <vladislav.andrs@gmail.com>
2017-12-20 10:47:21 +0100

    Clicking the button sets the whole month

--- app/components/c-datepicker-from-to.js
+++ app/components/c-datepicker-from-to.js
@@ -3,7 +3,8 @@
 shows two labels with calendar (from / to date)
 
 {{c-datepicker-from-to storage=session presets=true}}
-shows buttons '1.day of this month', '1.day of previous month'
+shows buttons 'this month', 'previous month',
+sets date from firs day of the month to the last day of the month
 */
 
 import Ember from 'ember';
@@ -32,9 +33,9 @@ export default Ember.Component.extend({
       var datetime = moment().format(); // Now
       // how many months back? Returns 1st of month or 1st of previous month or ...
       datetime = moment().subtract(monthsBack,'months').startOf('month').format();
-
+      // save start of month and end of month
       this.set('storage.datepickerDateFrom', moment(datetime).format('YYYY-MM-DD'));
-      this.set('storage.datepickerDateTo', moment(datetime).format('YYYY-MM-DD'));
+      this.set('storage.datepickerDateTo', moment(datetime).endOf('month').format('YYYY-MM-DD'));
       this.sendAction('dateChanged');
 
     },
